4747a649307e34e8ef612d8bac093c33e645b987
pnjia <panjiangkasaputra67@gmail.com>
2025-11-16 07:47:36 +0700

    Membuat seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        $categories = [
+            ['cat_name'=> 'Makanan', 'description' => 'Kategori Makanan'],
+            ['cat_name'=> 'Minuman', 'description' => 'Kategori Minuman'],
+
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+}
---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Item;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ItemSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        Item::factory(10)->create();
+    }
+}
