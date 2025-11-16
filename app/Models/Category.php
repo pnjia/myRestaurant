77d5e0c27af461f6c987201a6e350861eb907188
pnjia <panjiangkasaputra67@gmail.com>
2025-11-16 20:56:16 +0700

    Menambahkan fungsionalitas model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'cat_name', 
+        'description',
+        'created_at', 
+        'updated_at'
+    ];
+
+    protected $dates =  ['deleted_at'];
+
+    public function item() {
+        return $this->hasMany(Item::class);
+    }
 }
