8ebb508be9bedec1e61a2d76b8e55bfd5ddd8a02
pnjia <panjiangkasaputra67@gmail.com>
2025-11-16 21:00:13 +0700

    Menambahkan rute untuk menu, home, cart, checkout

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('customer.index');
+})->name('home');
+Route::get('/menu', function () {
+    return view('customer.menu');
+})->name('menu');
+Route::get('/cart', function () {
+    return view('customer.cart');
+})->name('cart');
+Route::get('/checkout', function () {
+    return view('customer.checkout');
+})->name('checkout');
