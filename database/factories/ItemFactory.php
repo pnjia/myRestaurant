588fa308d56783d3b7f06efa8da4ac120bc5d86d
pnjia <panjiangkasaputra67@gmail.com>
2025-11-18 08:40:15 +0700

    Memperbaiki img agar menggunakan url asli

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -17,7 +17,12 @@ class ItemFactory extends Factory
             'price' => fake()->randomFloat(2, 1000, 100000),
             'description' => fake()->text(),
             'category_id' => fake()->numberBetween(1, 2),
-            'img' => fake()->imageUrl(),
+            'img' => fake()->randomElement([
+                'https://images.unsplash.com/photo-1638866281450-3933540af86a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                'https://images.unsplash.com/photo-1632440722549-692fc6af969e?q=80&w=1207&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                'https://images.unsplash.com/photo-1527661591475-527312dd65f5?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+
+            ]),
             'is_active' => fake()->boolean()
         ];
     }
