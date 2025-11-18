5bda1aaf3241597b2a9ca08bd78fcc8e64a27d70
pnjia <panjiangkasaputra67@gmail.com>
2025-11-18 08:39:00 +0700

    Menambahkan controller ke rute menu

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('customer.index');
 })->name('home');
-Route::get('/menu', function () {
-    return view('customer.menu');
-})->name('menu');
+Route::get('/menu', [MenuController::class, 'index'])->name('menu');
 Route::get('/cart', function () {
     return view('customer.cart');
 })->name('cart');
